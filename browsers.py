a74b0317d34a130370ecf02dd51b725530ff78c4
atheodog <111013695+atheodog@users.noreply.github.com>
2022-11-23 14:07:21 -0300

    Add files via upload

---
+++ browsers.py
@@ -0,0 +1,265 @@
+import webbrowser
+
+def baidu():
+ webbrowser.open('https://baidu-browser.en.softonic.com/')
+
+def brave():
+ webbrowser.open('https://brave.com/download/')
+ 
+def bravenightly():
+ webbrowser.open('https://brave.com/download-nightly/')
+ 
+def bravebeta():
+ webbrowser.open('https://brave.com/download-beta/')
+ 
+def basilisk():
+ webbrowser.open('https://www.basilisk-browser.org/')
+ 
+def blisk():
+ webbrowser.open('https://blisk.io/')
+ 
+def beaker():
+ webbrowser.open('https://beakerbrowser.com/')
+
+def ungoogledchromium():
+ webbrowser.open('https://github.com/orgs/ungoogled-software/repositories')
+ 
+def chrome():
+ webbrowser.open('https://www.google.com/chrome/')
+ 
+def chromedev():
+ webbrowser.open('https://www.google.com/chrome/dev/')
+ 
+def chromebeta():
+ webbrowser.open('https://www.google.com/chrome/beta/')
+ 
+def chromecanary():
+ webbrowser.open('https://www.google.com/chrome/canary/')
+
+def coccoc():
+ webbrowser.open('https://coccoc.com/')
+ 
+def cent():
+ webbrowser.open('https://www.centbrowser.com/')
+
+def comododragon():
+ webbrowser.open('https://www.comodo.com/home/browsers-toolbars/browser.php')
+
+def comodoicedragon():
+ webbrowser.open('https://www.comodo.com/home/browsers-toolbars/browser.php')
+
+def cyberfox():
+ webbrowser.open('https://sourceforge.net/projects/cyberfox/')
+
+def dot():
+ webbrowser.open('https://www.dothq.co/en')
+ 
+def eldy():
+ webbrowser.open('http://www.eldy.eu/en/software/download/')
+ 
+def firefox():
+ webbrowser.open('https://www.mozilla.org/en-US/firefox/new/')
+ 
+def firefoxbeta():
+ webbrowser.open('https://www.mozilla.org/en-US/firefox/channel/desktop/')
+ 
+def firefoxdev():
+ webbrowser.open('https://www.mozilla.org/en-US/firefox/channel/desktop/')
+ 
+def firefoxnightly():
+ webbrowser.open('https://www.mozilla.org/en-US/firefox/channel/desktop/')
+ 
+def falkon():
+ webbrowser.open('https://www.falkon.org/')
+
+def ghost():
+ webbrowser.open('https://ghostbrowser.com/')
+
+def librewolf():
+ webbrowser.open('https://librewolf.net/')
+
+def maxthon():
+ webbrowser.open('https://www.maxthon.com/mx6/download/')
+
+def maxthonbeta():
+ webbrowser.open('https://www.maxthon.com/mx6/beta/')
+
+def maxthonnitro():
+ webbrowser.open('https://www.maxthon.com/nitro/static.html')
+
+def maxthonclassic():
+ webbrowser.open('https://www.mediafire.com/file/g764bdkhacu90w4/MAXTHON_CLASSIC_mx_1.6.7.34.exe/file')
+
+def midori():
+ webbrowser.open('https://astian.org/en/midori-browser-desktop/')
+
+def superbrowse():
+ webbrowser.open('https://superbrowse.en.uptodown.com/windows')
+ 
+def sidekick():
+ webbrowser.open('https://www.meetsidekick.com/')
+ 
+def tor():
+ webbrowser.open('https://www.torproject.org/')
+ 
+def seamonkey():
+ webbrowser.open('https://www.seamonkey-project.org/releases/')
+ 
+def tungsten():
+ webbrowser.open('https://en.tungsten-start.net/')
+ 
+def palemoon():
+ webbrowser.open('https://www.palemoon.org/')
+ 
+def otter():
+ webbrowser.open('https://otter-browser.org/')
+ 
+def opera():
+ webbrowser.open('https://www.opera.com/browsers/opera')
+ 
+def operabeta():
+ webbrowser.open('https://www.opera.com/browsers/opera/beta')
+ 
+def operadev():
+ webbrowser.open('https://www.opera.com/browsers/opera/beta')
+ 
+def operacrypto():
+ webbrowser.open('https://www.opera.com/crypto/next/')
+ 
+def operagx():
+ webbrowser.open('https://www.opera.com/gx')
+ 
+def minbrowser():
+ webbrowser.open('https://minbrowser.org/')
+ 
+def pulse():
+ webbrowser.open('https://pulsebrowser.app/')
+
+def waterfox():
+ webbrowser.open('https://www.waterfox.net/')
+ 
+def whale():
+ webbrowser.open('https://whale.naver.com/en/')
+
+def wyzo():
+ webbrowser.open('https://wyzo.en.uptodown.com/windows')
+ 
+def yandex():
+ webbrowser.open('https://browser.yandex.com/')
+ 
+def vivaldi():
+ webbrowser.open('https://vivaldi.com/')
+  
+def citrio():
+ webbrowser.open('https://citrio.com/')
+ 
+def osiris():
+ webbrowser.open('https://decenternet.net/osiris')
+ 
+def avant():
+ webbrowser.open('http://www.avantbrowser.com/')
+ 
+def avastbrowser():
+ webbrowser.open('https://www.avast.com/secure-browser#pc')
+ 
+def ghostlie():
+ webbrowser.open('https://sourceforge.net/projects/ghostlie/')
+ 
+def lightfirefox():
+ webbrowser.open('https://sourceforge.net/projects/lightfirefox/')
+ 
+def briskbard():
+ webbrowser.open('https://www.briskbard.com/index.php?lang=en&pageid=featweb')
+ 
+def chedot():
+ webbrowser.open('https://landing.chedot.com/')
+
+def chromodo():
+ webbrowser.open('https://help.comodo.com/topic-72-1-623-7595-.html#cd_starting')
+ 
+def bittube():
+ webbrowser.open('https://bittube.app/')
+ 
+def cryptotab():
+ webbrowser.open('https://cryptobrowser.site/en/')
+ 
+def operaneon():
+ webbrowser.open('https://www.opera.com/browsers/neon')
+ 
+def urbrowser():
+ webbrowser.open('https://www.ur-browser.com/en-US')
+ 
+def viasat():
+ webbrowser.open('https://browser.viasat.com/')
+ 
+def kinza():
+ webbrowser.open('https://www.kinza.jp/en/')
+ 
+def sleipnir():
+ webbrowser.open('https://www.fenrir-inc.com/jp/sleipnir/')
+
+def slimjet():
+ webbrowser.open('https://www.slimbrowser.net/en/dlpage.php')
+ 
+def qutebrowser():
+ webbrowser.open('https://github.com/qutebrowser/qutebrowser')
+ 
+def abrowser():
+ webbrowser.open('https://sourceforge.net/projects/abrowser/')
+ 
+def iridium():
+ webbrowser.open('https://iridiumbrowser.de/')
+
+def airbrowse():
+ webbrowser.open('https://sourceforge.net/projects/airbrowse3/')
+ 
+def slimboat():
+ webbrowser.open('https://slimboat.en.softonic.com/')
+
+def kingpin():
+ webbrowser.open('https://kingpinbrowser.com/') 
+
+def avgbrowser():
+ webbrowser.open('https://www.avg.com/en-us/secure-browser#pc') 
+
+def arora():
+ webbrowser.open('https://arora.en.softonic.com/download')
+ 
+def biscuit():
+ webbrowser.open('https://eatbiscuit.com/')
+ 
+def decentr():
+ webbrowser.open('https://decentr.net/')
+ 
+def sandcat():
+ webbrowser.open('https://www.syhunt.com/sandcat/')
+ 
+def ghostery():
+ webbrowser.open('https://www.ghostery.com/private-browser')
+ 
+def programmerbrowser():
+ webbrowser.open('https://github.com/yessGlory17/programmer-browser')
+ 
+def sielo():
+ webbrowser.open('https://sielo.app/en/index.html')
+ 
+def ccleaner():
+ webbrowser.open('https://www.ccleaner.com/ccleaner/browser')
+ 
+def atom():
+ webbrowser.open('https://browser.ru/#')
+  
+def itop():
+ webbrowser.open('https://www.itopvpn.com/vpn-browser')
+ 
+def netscape():
+ webbrowser.open('https://netscape-browser.en.softonic.com/')
+ 
+def quantum():
+ webbrowser.open('https://quantumbrowser.en.softonic.com/')
+ 
+def thorium():
+ webbrowser.open('https://thorium.rocks/')
+ 
+def yandexbeta():
+ webbrowser.open('https://browser.yandex.com/beta/')
